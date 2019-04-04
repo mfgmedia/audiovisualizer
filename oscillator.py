ce15be01a89c9cbffba6a70ef00bb6d602cb663c
drisme <drisme777@gmail.com>
2019-04-04 17:01:03 +0200

    added flashing background, curve now blood red

--- oscillator.py
+++ oscillator.py
@@ -5,26 +5,36 @@ import collections
 
 imWidth, imHeight = 1024, 512  # screen size
 
+buff = collections.deque([0.0] * 10, maxlen=10)
 
-buff = collections.deque([0.0]*10, maxlen=10)
 
+def draw_wave(screen, mono_audio, xs, title="oscilloscope", gain=1, print_avg=False):
+    # freq = find_freq(mono_audio[0:len(xs)], len(xs))
+    peak_volume = np.max(mono_audio[0:len(xs)])
+    buff.append(peak_volume)
+    avg = np.mean(buff)
+    weight = peak_volume + avg / 3.0
 
-def draw_wave(screen, mono_audio, xs, title="oscilloscope", gain=2):
-    # screen = screen * 1  # clear the screen
-    # print(np.max(screen))
-    # screen = np.multiply(screen, 1)
+    # flash background
+    cv2.addWeighted(screen, 1 - weight, np.full((imHeight, imWidth, 3), [200, 200, 255], dtype=np.uint8), weight, 1,
+                    screen)
+
+    # fade out image (multiply would be preferred but turns everything blue)
+    # cv2.multiply(screen, 0.66, screen)
+    cv2.addWeighted(screen, .8, np.zeros((imHeight, imWidth, 3), dtype=np.uint8), 0.2, 0, screen)
+
     ys = imHeight / 2 * (1 - np.clip(gain * mono_audio[0:len(xs)], -1, 1))  # the y-values of the waveform
     pts = np.array(list(zip(xs, ys))).astype(np.int)  # pair up xs & ys
-    cv2.polylines(screen, [pts], False, (0, 255, 255), thickness=18)  # connect points w/ lines
-    cv2.polylines(screen, [pts], False, (255, 0, 255), thickness=12)  # connect points w/ lines
-    cv2.polylines(screen, [pts], False, (255, 255, 0), thickness=4)
-    freq = find_volume(mono_audio[0:len(xs)], len(xs))
-    buff.append(freq)
-    avg = np.mean(buff)
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(screen, str(avg), (10, 500), font, 4, (255, 255, 0), 2, cv2.LINE_AA)
-    # cv2.line(screen, (0, 0), (511, 511), (255, 0, 0), 5)
+    cv2.polylines(screen, [pts], False, (200, 100, 255), thickness=18)  # connect points w/ lines
+    cv2.polylines(screen, [pts], False, (100, 50, 255), thickness=12)  # connect points w/ lines
+    cv2.polylines(screen, [pts], False, (0, 0, 255), thickness=4)
+
+    if print_avg:
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(screen, str(avg), (10, 500), font, 4, (255, 255, 0), 2, cv2.LINE_AA)
+
     cv2.imshow(title, screen)  # show what we've got
+    return screen
 
 
 def find_volume(np_arr, frate):
@@ -46,7 +56,7 @@ xs = np.arange(imWidth).astype(np.int)  # x values of pixels
 while (1):  # keep looping until someone stops this madness
     with default_mic.recorder(samplerate=44100) as mic:
         audio_data = mic.record(numframes=1024)  # get some audio from the mic
-    draw_wave(screen, audio_data[:, 0], xs)  # draw left channel
+    screen = draw_wave(screen, audio_data[:, 0], xs)  # draw left channel
     key = cv2.waitKey(1) & 0xFF  # keyboard input
     if ord('q') == key:  # quit key
         break
